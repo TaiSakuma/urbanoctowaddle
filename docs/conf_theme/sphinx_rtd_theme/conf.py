ba071fac679af6069d74a8dcae2d9bf62309d8c4
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-11 11:56:14 +0000

    configure for sphinx_rtd_theme

---
+++ docs/conf_theme/sphinx_rtd_theme/conf.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+
+html_theme_options = {
+    # 'typekit_id': hiw1hhg,
+    # 'canonical_url':
+    # 'analytics_id':
+    'collapse_navigation': False,
+    'sticky_navigation': False,
+    'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'logo_only':
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+}
+
+templates_path_theme = ['conf_theme/sphinx_rtd_theme/templates']
+html_static_path_theme = ['conf_theme/sphinx_rtd_theme/static']
+stylesheets_theme = [ ]
+
+##__________________________________________________________________||
