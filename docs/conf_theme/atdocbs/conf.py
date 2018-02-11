6640f3774003add9123fb2f4952da2cc8e6a5b39
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-11 09:54:19 +0000

    add conf for atdocbs

---
+++ docs/conf_theme/atdocbs/conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import atdocbs
+html_theme = 'atdocbs'
+html_theme_path = atdocbs.path()
+print(html_theme_path)
+
+html_theme_options = {
+}
+
+templates_path_theme = ['conf_theme/atdocbs/templates']
+html_static_path_theme = ['conf_theme/atdocbs/static']
+stylesheets_theme = [ ]
+
+##__________________________________________________________________||
